c1694b0a7368289d167ce79d500bbbbf226d2d77
Venj Chu <ersaclarke@gmail.com>
2016-03-31 21:39:21 +0800

    Missing a file

---
+++ Welding Recorder/ControlExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Welding_Recorder
+{
+    public static class ControlExtensions
+    {
+        public static void UIThread(this Control @this, Action code)
+        {
+            if (@this.InvokeRequired)
+            {
+                @this.BeginInvoke(code);
+            }
+            else
+            {
+                code.Invoke();
+            }
+        }
+    }
+}
